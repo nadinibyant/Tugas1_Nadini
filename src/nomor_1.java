ca63e17233cf620dadbbe2fe557ddf67faffd6a5
Nadini Annisa Byant <89978510+nadinibyant@users.noreply.github.com>
2022-02-25 06:54:17 +0700

    Update nomor_1.java

--- src/nomor_1.java
+++ src/nomor_1.java
@@ -1,20 +1,10 @@
-
-// package untuk akses araylist
-import java.util.ArrayList;
-
 public class nomor_1 {
     public static void main(String[] args) {
         String[] nama = { "b", "y", "a", "n" };
 
-        // masukan data nama ke list array baru (nama2) -> bisa akses method
-        ArrayList<String> nama2 = new ArrayList<>();
-        for (int i = 0; i < nama.length; i++) {
-            nama2.add(nama[i]);
-        }
-
         // IsEmpty()
         for (int i = 0; i < nama.length; i++) {
-            System.out.println("data index ke-" + i + " : " + nama2.isEmpty());
+            System.out.println("data index ke-" + i + " : " + nama[i].isEmpty());
         }
 
     }
